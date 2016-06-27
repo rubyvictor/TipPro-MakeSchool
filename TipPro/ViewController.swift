08dc34f0524560083de57d0f2bf633819f6e9057
Victor Lee <myleevictor@gmail.com>
2016-06-27 10:43:13 +0800

    First Commit to Master

---
+++ TipPro/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  TipPro
+//
+//  Created by Victor Lee on 21/6/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelectorField: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        guard let billAmount = Double(billAmountField.text!) else {
+            
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+            return
+        }
+    
+        var tipPercentage = 0.0
+        
+        switch tipSelectorField.selectedSegmentIndex {
+            
+        case 0:
+            tipPercentage = 0.15
+        case 1:
+            tipPercentage = 0.18
+        case 2:
+            tipPercentage = 0.20
+        default:
+            break
+        }
+    
+        let roundedBillAmount = round(100 * billAmount)/100
+        let tipAmount = roundedBillAmount * tipPercentage
+        let roundedTipAmount = round(100 * tipAmount)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if (!billAmountField.editing) {
+            billAmountField.text = String(format: "%.2f", roundedBillAmount)
+        }
+        tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountField.text = String(format: "%.2f", totalAmount)
+        
+    }
+    
+}
+
